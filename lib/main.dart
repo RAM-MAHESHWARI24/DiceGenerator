8a99e095d055fd6e91dca30c8978be784f96d8ee
Legion <HEMONESH@GMAIL.com>
2024-07-05 00:52:06 +0530

    modified:   lib/gradient_container.dart 	modified:   lib/main.dart 	modified:   pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,11 @@ import 'package:flutter_application_1/gradient_container.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
           body: GradientContainer(
-        'Hello wrld!!',
-        Color.fromARGB(255, 20, 2, 80),
-        Color.fromARGB(255, 69, 7, 98),
+        const Color.fromARGB(255, 40, 4, 133),
+        const Color.fromARGB(255, 69, 7, 98),
       )),
     ),
   );
